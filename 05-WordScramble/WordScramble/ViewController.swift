1c11750ce046a5d89c2ab8dd233a324c8f12def9
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-05 11:12:25 +0100

    Initial empty version of WordScramble

---
+++ 05-WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Benjamin van den Hout on 05/11/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
