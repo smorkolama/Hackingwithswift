6b2181dcfa801f416360fd635409bf2e3be0c4a3
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-07 15:15:32 +0100

    Basic 07-WhitehousePetitions that retrieves JSON from URL with Codable

---
+++ 07-WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Benjamin van den Hout on 07/11/2019.
+//  Copyright © 2019 NGTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) { // might throw an error so use try?
+                // Ready to parse
+                parse(json: data)
+            }
+        }
+    }
+
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+
+
+    // MARK: tableview
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                print("count: \(petitions.count)")
+                return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+
+}
+
