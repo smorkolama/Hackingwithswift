7bdb6374e071262cde7e9d5179e9eab67450afb1
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-19 16:12:26 +0100

    Clone 01-StormViewer for GCD

---
+++ 09-StormViewerGCD/StormViewer/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Benjamin van den Hout on 24/10/2019.
+//  Copyright © 2019 NGTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true // try to set to false to see the difference, only recommended for first screen
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // load picture
+                pictures.append(item)
+            }
+        }
+//        print(pictures)
+        pictures.sort()
+    }
+
+    // MARK: Tableview
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.imageTitle = "\(pictures[indexPath.row]) (\(indexPath.row+1) of \(pictures.count))"
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
