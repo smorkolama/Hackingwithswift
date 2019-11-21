e6a7568e9b213004f11929a0e5e931b368fcf414
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-21 14:26:24 +0100

    Initial commit of 10-NamesToFaces

---
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Benjamin van den Hout on 21/11/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
