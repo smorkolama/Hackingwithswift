69728aa5c642b1456fb692ae5e87e8a39803792b
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-21 15:04:07 +0100

    Initial cells added, not showing labels yet and seems a bit small

---
+++ 10-NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Benjamin van den Hout on 21/11/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK: - UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        
+        return cell
+    }
+}
+
