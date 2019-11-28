2a08e423cfb3aeeb49c9ab533bb6df832e2590c4
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-28 16:04:52 +0100

    add particile emitter

--- 11-Pachinko/Pachinko/GameScene.swift
+++ 11-Pachinko/Pachinko/GameScene.swift
@@ -159,6 +159,11 @@ class GameScene: SKScene {
     }
     
     func destroy(ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+
         ball.removeFromParent()
     }
 }
