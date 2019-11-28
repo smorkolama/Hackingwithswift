9b4bf43a7418ddfbd8b031a8933c0b958b1c4f27
Benjamin van den Hout <benjamin@ngti.nl>
2019-11-28 14:05:56 +0100

    11-Pachinko empty state

---
+++ 11-Pachinko/Pachinko/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Pachinko
+//
+//  Created by Benjamin van den Hout on 28/11/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }    
+}
