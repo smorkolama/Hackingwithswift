0742ce74947cb2d5c59e4c18dcdc9af9fde7ea53
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-05 14:11:43 +0100

    Add sepia filter

--- 13-Instafilter/Instafilter/ViewController.swift
+++ 13-Instafilter/Instafilter/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+
+    var currentImage: UIImage!
+    // CoreImage stuff
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
+    @IBAction func changeFilterPressed(_ sender: Any) {
+    }
+    
+    @IBAction func savePressed(_ sender: Any) {
+    }
 
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        // read output image from current filter
+        guard let image = currentFilter.outputImage else { return }
+
+        // set filter intensity 0...1
+        currentFilter.setValue(slider.value, forKey: kCIInputIntensityKey)
+        
+        // create new image based on current image, image.extent = all of the image
+        if let cgimg = context.createCGImage(image, from: image.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
+}
