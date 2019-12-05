cb7b47decef15106abe50a84f366843455ff8e9d
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-05 13:39:11 +0100

    Optional 2: highscore for 01-GuessTheFlag, add 13-Instafilter base project

---
+++ 13-Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Benjamin van den Hout on 05/12/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
