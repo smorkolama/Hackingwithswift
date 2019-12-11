3b1e5454798c883ed958f8eaeaf5b5b905f79f59
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-11 12:57:44 +0100

    Penguins popping up and hiding

--- 14-Whack-a-Penguin/Whack-a-Penguin/GameViewController.swift
+++ 14-Whack-a-Penguin/Whack-a-Penguin/GameViewController.swift
@@ -18,8 +18,8 @@ class GameViewController: UIViewController {
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                // Fix for ipad pro, just stretch a little for ipad pro 11-inch (different aspect ratio than)
+                scene.scaleMode = .fill
                 
                 // Present the scene
                 view.presentScene(scene)
