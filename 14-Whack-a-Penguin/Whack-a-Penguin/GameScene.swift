30dd8cb1622bfe3e44a1071f5e51e085b5d56787
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-11 12:01:16 +0100

    Empty project for 14-Whack-a-Penguin

---
+++ 14-Whack-a-Penguin/Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by Benjamin van den Hout on 11/12/2019.
+//  Copyright © 2019 NGTI. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
