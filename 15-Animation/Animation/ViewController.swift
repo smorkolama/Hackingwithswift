349122b6dd517440230685855f0d740577b204a7
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-12 10:41:39 +0100

    15-Animation finished

--- 15-Animation/Animation/ViewController.swift
+++ 15-Animation/Animation/ViewController.swift
@@ -11,28 +11,50 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var button: UIView!
-    var imageView: UIImageView!
     var currentAnimation = 0
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        imageView = UIImageView(image: UIImage(named: "penguin"))
-        imageView.center = CGPoint(x: 512, y: 384)
+        imageView.image = UIImage(named: "penguin")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ]);
         view.addSubview(imageView)
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isHidden = true
 
-        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+//        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
             switch self.currentAnimation {
             case 0:
-                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2) // make bigger
                 break
 
-            case 1:
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256) // move
+                
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi) // pi is 180 degrees
+                
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.green
+                
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = UIColor.clear
+                
+            case 1,3,5:
                 self.imageView.transform = .identity // reset any changes
+                
+
             default:
                 break
             }
