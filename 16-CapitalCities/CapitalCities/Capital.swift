5751f95cd3592a360d51692d2e42bcc486be79f7
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-12 11:46:31 +0100

    16-CapitalCities complete

---
+++ 16-CapitalCities/CapitalCities/Capital.swift
@@ -0,0 +1,25 @@
+//
+//  Capital.swift
+//  CapitalCities
+//
+//  Created by Benjamin van den Hout on 12/12/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+// Notice that this is not a subclass, it implements MKAnnotation protocol!
+class Capital: NSObject, MKAnnotation {
+    init(title: String?, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    
+}
---
+++ 16-CapitalCities/CapitalCities/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Benjamin van den Hout on 12/12/2019.
+//  Copyright © 2019 Benjamin van den Hout. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 summer olympics")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a a thousand years ago")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called City of Light")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself")
+
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Will also get notified for other stuff, only react on Capital objects
+        guard annotation is Capital else { return nil }
+        
+        // re-use identifier
+        let identifier = "Capital"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        // Could be that we need to construct our own
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
+    
+    // This gets called automatically by the (I) button in annotation, no need to add targets
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+}
+
