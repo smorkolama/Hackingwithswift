39594024dabcbb073dcb037da1e2ad376c6e5e34
Benjamin van den Hout <benjamin@ngti.nl>
2019-12-19 14:54:43 +0100

    Empty project

---
+++ 20-FireworksNight/FireworksNight/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  FireworksNight
+//
+//  Created by Benjamin van den Hout on 19/12/2019.
+//  Copyright © 2019 NGTI. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+}
