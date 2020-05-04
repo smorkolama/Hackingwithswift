42be777024c21e3bce95679ceea74b07078a7b97
Benjamin van den Hout <benjamin@ngti.nl>
2020-05-04 15:39:13 +0200

    Base project

---
+++ 23-SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  SwiftyNinja
+//
+//  Created by Benjamin van den Hout on 04/05/2020.
+//  Copyright © 2020 Benjamin van den Hout. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
